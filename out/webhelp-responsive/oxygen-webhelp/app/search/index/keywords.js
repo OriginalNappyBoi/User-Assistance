a27db218dc7eb6bbf0f21d55ef55225dcc5b3cbd
briannamcbride <briannamcbride@10.77.173.213>
2019-12-02 21:14:11 -0500

    Updates, including a few small grammar things on random topics. -Bri

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"Adding",p:["p0","p1","p2","p3","p4"]},{w:"an",p:["p0","p8"]},{w:"attachment",p:["p0"]},{w:"a",p:["p1","p2","p3","p4","p5","p6","p7","p11"]},{w:"card",p:["p1","p6","p14"]},{w:"label",p:["p2","p5"]},{w:"list",p:["p3"]},{w:"sticker",p:["p4"]},{w:"Assigning",p:["p5"]},{w:"Commenting",p:["p6"]},{w:"on",p:["p6"]},{w:"Creating",p:["p7","p8"]},{w:"checklist",p:["p7"]},{w:"account",p:["p8"]},{w:"Downloading",p:["p9"]},{w:"Desktop",p:["p9","p10"]},{w:"Trello",p:["p9","p16"]},{w:"app",p:["p9","p10"]},{w:"for",p:["p9","p10","p14"]},{w:"Mac",p:["p9"]},{w:"Download",p:["p10"]},{w:"the",p:["p10"]},{w:"Trello-PC",p:["p10"]},{w:"instructions",p:["p10"]},{w:"Inviting",p:["p11"]},{w:"member",p:["p11"]},{w:"Kanban",p:["p12"]},{w:"method",p:["p12","p19","p20"]},{w:"Labeling",p:["p13"]},{w:"system",p:["p13"]},{w:"Markdown",p:["p14"]},{w:"syntax",p:["p14"]},{w:"descriptions",p:["p14"]},{w:"Power-ups",p:["p15"]},{w:"Types",p:["p16"]},{w:"of",p:["p16"]},{w:"Accounts",p:["p16"]},{w:"Printing",p:["p17"]},{w:"and",p:["p17"]},{w:"Exporting",p:["p17"]},{w:"Searching",p:["p18"]},{w:"within",p:["p18"]},{w:"boards",p:["p18"]},{w:"Social",p:["p19"]},{w:"media",p:["p19"]},{w:"Time",p:["p20"]}];
+define(function() {var keywords=[{w:"Adding",p:["p0","p1","p2","p3","p4"]},{w:"an",p:["p0","p8"]},{w:"attachment",p:["p0"]},{w:"a",p:["p1","p2","p3","p4","p5","p6","p7","p12","p14"]},{w:"card",p:["p1","p6","p12","p17"]},{w:"label",p:["p2","p5"]},{w:"list",p:["p3"]},{w:"sticker",p:["p4"]},{w:"Assigning",p:["p5"]},{w:"Commenting",p:["p6"]},{w:"on",p:["p6"]},{w:"Creating",p:["p7","p8"]},{w:"checklist",p:["p7"]},{w:"account",p:["p8"]},{w:"Downloading",p:["p9"]},{w:"Desktop",p:["p9","p10"]},{w:"Trello",p:["p9","p13","p19"]},{w:"app",p:["p9","p10"]},{w:"for",p:["p9","p10","p17"]},{w:"Mac",p:["p9"]},{w:"Download",p:["p10"]},{w:"the",p:["p10"]},{w:"Trello-PC",p:["p10"]},{w:"instructions",p:["p10"]},{w:"Getting",p:["p11"]},{w:"Started",p:["p11"]},{w:"Using",p:["p12"]},{w:"Introduction",p:["p13"]},{w:"to",p:["p13"]},{w:"Inviting",p:["p14"]},{w:"member",p:["p14"]},{w:"Kanban",p:["p15"]},{w:"method",p:["p15","p22","p23"]},{w:"Labeling",p:["p16"]},{w:"system",p:["p16"]},{w:"Markdown",p:["p17"]},{w:"syntax",p:["p17"]},{w:"descriptions",p:["p17"]},{w:"Power-ups",p:["p18"]},{w:"Types",p:["p19"]},{w:"of",p:["p19"]},{w:"Accounts",p:["p19"]},{w:"Printing",p:["p20"]},{w:"and",p:["p20"]},{w:"Exporting",p:["p20"]},{w:"Searching",p:["p21"]},{w:"within",p:["p21"]},{w:"boards",p:["p21"]},{w:"Social",p:["p22"]},{w:"media",p:["p22"]},{w:"Time",p:["p23"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[0, 3, 4];
@@ -11,16 +11,19 @@ ph["p7"]=[11, 3, 12];
 ph["p8"]=[11, 1, 13];
 ph["p9"]=[14, 15, 16, 17, 18, 19];
 ph["p10"]=[20, 21, 15, 17, 18, 22, 23];
-ph["p20"]=[45, 27];
-ph["p12"]=[26, 27];
-ph["p11"]=[24, 3, 25];
-ph["p14"]=[30, 31, 18, 4, 32];
-ph["p13"]=[28, 29];
-ph["p16"]=[34, 35, 16, 36];
-ph["p15"]=[33];
-ph["p18"]=[40, 41, 42];
-ph["p17"]=[37, 38, 39];
-ph["p19"]=[43, 44, 27];
+ph["p21"]=[45, 46, 47];
+ph["p20"]=[42, 43, 44];
+ph["p12"]=[26, 3, 4];
+ph["p23"]=[50, 32];
+ph["p11"]=[24, 25];
+ph["p22"]=[48, 49, 32];
+ph["p14"]=[29, 3, 30];
+ph["p13"]=[27, 28, 16];
+ph["p16"]=[33, 34];
+ph["p15"]=[31, 32];
+ph["p18"]=[38];
+ph["p17"]=[35, 36, 18, 4, 37];
+ph["p19"]=[39, 40, 16, 41];
      return {
          keywords: keywords,
          ph: ph
