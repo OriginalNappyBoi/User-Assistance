5e88c3874673bc31e01bfd585adb23f16917be76
briannamcbride <briannamcbride@10.77.173.213>
2019-12-04 13:35:56 -0500

    Updates. -Bri

---
+++ out/webhelp-responsive/oxygen-webhelp/app/nav-links/json/concept_u11_wnj_sjb-d62e343.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Adding a power-up","shortdesc":"<p class=\"shortdesc\">Use power-ups to  make the board more efficient and effective.</p>","href":"adding-power-up-tsk.html","attributes": {"data-id":"task_i5j_pqj_sjb",},"menu": {"hasChildren":false,},"tocID":"task_i5j_pqj_sjb-d62e354","topics":[]}]});
--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {2:6,15:12,8:13,0:13,6:13,21:13,12:-1,5:17,14:-1,13:-1,9:12,7:13,18:13};
+return {};
 });
